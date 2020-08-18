2675920e71cdeb0c404a3333eec6b536a16b6739
yerx <yer.x.xiong@gmail.com>
2020-08-18 09:12:24 -0400

    added react routing for channels

--- react-messaging-app/src/App.js
+++ react-messaging-app/src/App.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import './App.css';
-import Header from  './components/Header';
-import Sidebar from './components/Sidebar';
+import React from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <div className="app__body">
-        <Sidebar />
-        {/* React-Router -> Chat screen */}
-      </div>
+    <div className="app">
+      <Router>
+        <Header />
+        <div className="app__body">
+          <Sidebar />
 
+          <Switch>
+            <Route path="/channel/:channelId">
+              <h1>CHAT SCREEN</h1>
+              {/* <Chat /> */}
+            </Route>
+            <Route>
+              <h1>Hello world</h1>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
