7f9838323642ca93ff71af195987831e6adfb98b
yerx <yer.x.xiong@gmail.com>
2020-08-18 08:54:06 -0400

    added useState and useEffect to map through channels collection and return the channels on the sidebar

--- react-messaging-app/src/components/Sidebar.js
+++ react-messaging-app/src/components/Sidebar.js
@@ -1,20 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import "./Sidebar.css";
-import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
-import CreateIcon from '@material-ui/icons/Create';
-import InsertCommentIcon from '@material-ui/icons/InsertComment';
-import SidebarOption from './SidebarOption';
-import InboxIcon from '@material-ui/icons/Inbox';
-import DraftsIcon from '@material-ui/icons/Drafts';
-import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
-import PeopleAltIcon from '@material-ui/icons/PeopleAlt';
-import AppsIcon from '@material-ui/icons/Apps';
-import FileCopyIcon from '@material-ui/icons/FileCopy';
-import ExpandLessIcon from '@material-ui/icons/ExpandLess';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import AddIcon from '@material-ui/icons/Add';
+import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord";
+import CreateIcon from "@material-ui/icons/Create";
+import InsertCommentIcon from "@material-ui/icons/InsertComment";
+import SidebarOption from "./SidebarOption";
+import InboxIcon from "@material-ui/icons/Inbox";
+import DraftsIcon from "@material-ui/icons/Drafts";
+import BookmarkBorderIcon from "@material-ui/icons/BookmarkBorder";
+import PeopleAltIcon from "@material-ui/icons/PeopleAlt";
+import AppsIcon from "@material-ui/icons/Apps";
+import FileCopyIcon from "@material-ui/icons/FileCopy";
+import ExpandLessIcon from "@material-ui/icons/ExpandLess";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import AddIcon from "@material-ui/icons/Add";
+import db from "../firebase.js";
 
 function Sidebar() {
+  const [channels, setChannels] = useState([]);
+
+  useEffect(() => {
+    // Run this code once when the sidebare component loads
+    db.collection("channels").onSnapshot((snapshot) =>
+      setChannels(
+        snapshot.docs.map((doc) => ({
+          id: doc.id,
+          name: doc.data().name,
+        }))
+      )
+    );
+  }, []);
+
   return (
     <div className="sidebar">
       <div className="sidebar__header">
@@ -26,7 +41,6 @@ function Sidebar() {
           </h3>
         </div>
         <CreateIcon />
-
       </div>
       <SidebarOption Icon={InsertCommentIcon} title="Threads" />
       <SidebarOption Icon={InboxIcon} title="Mentions & reactions" />
@@ -42,9 +56,12 @@ function Sidebar() {
       <SidebarOption Icon={AddIcon} addChannelOption title="Add Channel" />
 
       {/* Connect to db and list all the channels */}
+      {channels.map(channel => (
+        <SidebarOption title={channel.name} id={channel.id} />
+      ))}
       {/* <SidebarOption .../> */}
     </div>
-  )
+  );
 }
 
 export default Sidebar;
