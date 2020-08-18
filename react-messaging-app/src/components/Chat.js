6465efc502fda68c0398dd27ecfb782631b06d3c
yerx <yer.x.xiong@gmail.com>
2020-08-18 10:31:13 -0400

    implemented functionality to change the url when a channel is clicked on, make a call to the database using the url param, and fetch the channel details from the database

--- react-messaging-app/src/components/Chat.js
+++ react-messaging-app/src/components/Chat.js
@@ -1,18 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Chat.css";
 import { useParams } from "react-router-dom";
 import StarBorderOutlinedIcon from "@material-ui/icons/StarBorderOutlined";
 import InfoOutlinedIcon from "@material-ui/icons/InfoOutlined";
+import db from "../firebase.js";
 
 function Chat() {
   const { channelId } = useParams();
+  const [channelDetails, setChannelDetails] = useState(null);
+
+  useEffect(() => {
+    if (channelId) {
+      db.collection("channels")
+        .doc(channelId)
+        .onSnapshot((snapshot) => {
+          setChannelDetails(snapshot.data());
+        });
+    }
+  }, [channelId]);
+
+  console.log(channelDetails);
 
   return (
     <div className="chat">
       <div className="chat__header">
         <div className="chat__headerLeft">
           <h4 className="chat__channelName">
-            <strong># general</strong>
+            <strong>#{channelDetails?.name}</strong>
             <StarBorderOutlinedIcon />
           </h4>
         </div>
